4927b9b553f14c129ecd27720d13dfdc4702c93a
TimothyCruzCk12 <timothy.cruz.intern@ck12.org>
2025-08-18 09:48:51 -0700

    added app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import TimeTo from './components/TimeTo'
+
+
+function App() {
+  return (
+      <div className="App">
+        <TimeTo />
+      </div>
+  );
+}
+
+
+export default App;
